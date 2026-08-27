e91a008b61f4807ed6ad3fbdb23da66bc8b9ef9e
Hos_Hos <109749794+Houssem028@users.noreply.github.com>
2026-08-27 22:07:32 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="dabber-v1";
+const ASSETS=["./","./index.html","./style.css","./manifest.json","./logo.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
